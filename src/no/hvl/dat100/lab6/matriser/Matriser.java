591c37d9a552c58220c8a85b995108de54d7014c
mariuslarsen <mariuslarsen@mariuss-mbp.enivest.no>
2021-09-27 13:54:38 +0200

    Finished mandatory assignments

--- src/no/hvl/dat100/lab6/matriser/Matriser.java
+++ src/no/hvl/dat100/lab6/matriser/Matriser.java
@@ -4,32 +4,51 @@ public class Matriser {
 
 	// a)
 	public static void skrivUt(int[][] matrise) {
-		
-		// TODO
-		throw new UnsupportedOperationException("skrivUt ikke implementert");
+		for (int[] row : matrise) {
+			for (int x : row) {
+				System.out.print(x + " ");
+			}
+			System.out.println();
+		}
 	}
 
 	// b)
 	public static String tilStreng(int[][] matrise) {
-
-		// TODO
-		throw new UnsupportedOperationException("tilStreng ikke implementert");
-		
+		String stringRep = "";
+		for (int[] row : matrise) {
+			for (int x : row) {
+				stringRep += x + " ";
+			}
+			stringRep += "\n";
+		}
+	
+		return stringRep;
 	}
 
 	// c)
 	public static int[][] skaler(int tall, int[][] matrise) {
+		int[][] newMatrix = new int[matrise.length][matrise[0].length];
 		
-		// TODO
-		throw new UnsupportedOperationException("skaler ikke implementert");
-	
+		for (int i = 0; i < matrise.length; i++) {
+			for (int j = 0; j < matrise[i].length; j++) {
+				newMatrix[i][j] = matrise[i][j] * tall;
+			}
+		}
+		
+		return newMatrix;
 	}
 
 	// d)
 	public static boolean erLik(int[][] a, int[][] b) {
-
-		// TODO
-		throw new UnsupportedOperationException("erLik ikke implementert");
+		for (int i = 0; i < a.length; i++) {
+			for (int j = 0; j < a[i].length;j++) {
+				if (a[i][j] != b[i][j]) {
+					return false;
+				}
+			}
+		}
+		
+		return true;
 	}
 	
 	// e)
